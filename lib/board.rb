eb0022a87d453d7472d1eda0c169b042ec9de63a
Matthew Kimball <mekimba6@gmail.com>
2021-07-15 16:32:54 -0600

    adds test and method for valid coordinate

--- lib/board.rb
+++ lib/board.rb
@@ -2,6 +2,8 @@ require './lib/cell'
 require "pry"
 class Board
 
+
+
   def cells
   letters = ("A".."D")
     letters_array = []
@@ -17,7 +19,13 @@ class Board
       letters_array.map do |a|
         coord_hash[a] = Cell.new(a)
       end
+      coord_hash
   end
-binding.pry
+
+  def valid_coordinate?(coordinate)
+    cells.include?(coordinate)
+  end
+
+# binding.pry
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,6 +6,17 @@ RSpec.describe Board do
   it 'is a board' do
     board = Board.new
     expect(board).to be_a(Board)
-    binding.pry
+    expect(board.cells).to be_a(Hash)
+    # binding.pry
+  end
+
+  it 'checks valid coordinates' do
+    board = Board.new
+    # binding.pry
+    expect(board.valid_coordinate?("A1")).to eq(true)
+    expect(board.valid_coordinate?("D4")).to eq(true)
+    expect(board.valid_coordinate?("A5")).to eq(false)
+    expect(board.valid_coordinate?("E1")).to eq(false)
+    expect(board.valid_coordinate?("A22")).to eq(false)
   end
 end
