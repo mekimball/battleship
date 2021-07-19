a9a203a2c2cda507f3350b055e7e7a194e283e3c
Matthew Kimball <mekimba6@gmail.com>
2021-07-19 12:16:15 -0600

    fixes indentation

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,7 @@ require './lib/cell'
 
 class Board
 
-attr_reader :cells,:rows, :colums
+  attr_reader :cells,:rows, :colums
 
   def initialize
     @cells = {}
@@ -10,47 +10,47 @@ attr_reader :cells,:rows, :colums
     end
 
   def create_cell
-    rows =["A","B", "C", "D"]
-    colums = ["1", "2", "3", "4"]
+    rows = ("A".."D")
+    columns = ("1".."4")
     rows.each do |letter|
-    colums.each do |number|
-    coordinates = letter+number
-    @cells[coordinates] = Cell.new(coordinates)
-        end
+      columns.each do |number|
+      coordinates = letter+number
+      @cells[coordinates] = Cell.new(coordinates)
       end
     end
+  end
 
- def valid_coordinate?(cell)
+  def valid_coordinate?(cell)
    @cells.include?(cell)
   end
 
-def letters_map(coordinates)
-  coordinates.map do |coordinate|
-  coordinate[0].ord
-end
-end
+  def letters_map(coordinates)
+    coordinates.map do |coordinate|
+      coordinate[0].ord
+    end
+  end
 
-def numbers_map(coordinates)
-  coordinates.map do |coordinate|
-     coordinate[1].to_i
- end
-end
+  def numbers_map(coordinates)
+    coordinates.map do |coordinate|
+      coordinate[1].to_i
+   end
+  end
 
-def valid_placement?(ship,coordinates)
-  letters =letters_map(coordinates)
-  numbers =numbers_map(coordinates)
- if valid_size?(ship,coordinates) && availible(coordinates) && letters.uniq.count == 1 && numbers.each_cons(2).all? {|a,b| b == a + 1}
-   true
- elsif valid_size?(ship,coordinates) && availible(coordinates) && numbers.uniq.count == 1 && letters.each_cons(2).all? {|a,b| b == a + 1}
-  true
-else
-  false
-end
-end
+  def valid_placement?(ship,coordinates)
+    letters =letters_map(coordinates)
+    numbers =numbers_map(coordinates)
+    if valid_size?(ship,coordinates) && availible(coordinates) && letters.uniq.count == 1 && numbers.each_cons(2).all? {|a,b| b == a + 1}
+        true
+      elsif valid_size?(ship,coordinates) && availible(coordinates) && numbers.uniq.count == 1 && letters.each_cons(2).all? {|a,b| b == a + 1}
+        true
+      else
+        false
+    end
+  end
 
-def place (ship, coordinates)
-  coordinates.each do |coordinate|
-  @cells[coordinate].place_ship(ship)
+  def place (ship, coordinates)
+    coordinates.each do |coordinate|
+    @cells[coordinate].place_ship(ship)
     end
   end
 
@@ -59,30 +59,31 @@ def place (ship, coordinates)
     coordinates.all? do|coordinate|
       #using the empty method created to determine if a ship if there or not, if all coords are empty then they are availible
     @cells[coordinate].empty?
-  end
-  end
-  def valid_size?(ship,coordinates)
-  ship.length == coordinates.length
-end
-
-def render(arg = false)
-  board = "  1 2 3 4 \n" +
- "A #{@cells["A1"].render} #{@cells["A2"].render} #{@cells["A3"].render} #{@cells["A4"].render} \n" +
- "B #{@cells["B1"].render} #{@cells["B2"].render} #{@cells["B3"].render} #{@cells["B4"].render} \n" +
- "C #{@cells["C1"].render} #{@cells["C2"].render} #{@cells["C3"].render} #{@cells["C4"].render} \n" +
- "D #{@cells["D1"].render} #{@cells["D2"].render} #{@cells["D3"].render} #{@cells["D4"].render} \n"
-
- board_2 = "  1 2 3 4 \n" +
- "A #{@cells["A1"].render(true)} #{@cells["A2"].render(true)} #{@cells["A3"].render(true)} #{@cells["A4"].render(true)} \n" +
- "B #{@cells["B1"].render(true)} #{@cells["B2"].render(true)} #{@cells["B3"].render(true)} #{@cells["B4"].render(true)} \n" +
- "C #{@cells["C1"].render(true)} #{@cells["C2"].render(true)} #{@cells["C3"].render(true)} #{@cells["C4"].render(true)} \n" +
- "D #{@cells["D1"].render(true)} #{@cells["D2"].render(true)} #{@cells["D3"].render(true)} #{@cells["D4"].render(true)} \n"
-
- if arg == true
-   board_2
- else
-   board
     end
+  end
+
+  def valid_size?(ship,coordinates)
+    ship.length == coordinates.length
+  end
+
+  def render(arg = false)
+    board = "  1 2 3 4 \n" +
+   "A #{@cells["A1"].render} #{@cells["A2"].render} #{@cells["A3"].render} #{@cells["A4"].render} \n" +
+   "B #{@cells["B1"].render} #{@cells["B2"].render} #{@cells["B3"].render} #{@cells["B4"].render} \n" +
+   "C #{@cells["C1"].render} #{@cells["C2"].render} #{@cells["C3"].render} #{@cells["C4"].render} \n" +
+   "D #{@cells["D1"].render} #{@cells["D2"].render} #{@cells["D3"].render} #{@cells["D4"].render} \n"
+
+   board_2 = "  1 2 3 4 \n" +
+   "A #{@cells["A1"].render(true)} #{@cells["A2"].render(true)} #{@cells["A3"].render(true)} #{@cells["A4"].render(true)} \n" +
+   "B #{@cells["B1"].render(true)} #{@cells["B2"].render(true)} #{@cells["B3"].render(true)} #{@cells["B4"].render(true)} \n" +
+   "C #{@cells["C1"].render(true)} #{@cells["C2"].render(true)} #{@cells["C3"].render(true)} #{@cells["C4"].render(true)} \n" +
+   "D #{@cells["D1"].render(true)} #{@cells["D2"].render(true)} #{@cells["D3"].render(true)} #{@cells["D4"].render(true)} \n"
+
+     if arg == true
+       board_2
+     else
+       board
+     end
   end
 end
 
--- runner.rb
+++
@@ -1,5 +0,0 @@
-require './lib/board'
-require './lib/game'
-
-game = Game.new
-game.start
