13e3d51b114d62064b133cd585f1f9ba8617095e
Erin Quinn <equinn125@gmail.com>
2021-07-20 19:31:38 -0600

    update runner

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -1,1 +1,10 @@
+require './lib/game'
+require './lib/board'
+require './lib/cell'
+require './lib/ship'
 
+board = Board.new
+cruiser = Ship.new('cruiser', 3)
+submarine = Ship.new('submarine', 2)
+game = Game.new
+game.start
